165af7ca7316744e332b1c63e2beaaf2e805ac86
Alina Lemishko <aliska.nofear@gmail.com>
2022-05-26 16:35:13 +0300

    add js for gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// VARIABLES
+
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryGrid(items) {
+ return items.map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a href="" class="gallery__link">
+        <img class="gallery__image" src='${preview}' alt="${description}" >
+      </a>
+    </div>
+    `;
+    })
+    .join('');
+  
+  
+  };
+  
+
+galleryEl.insertAdjacentHTML('beforeend', makeGalleryGrid(galleryItems));
+galleryEl.addEventListener('click', onGalleryCardClick);
+
+
+function onGalleryCardClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+    
+  const imageOriginal = galleryItems.map(el => el.original);
+
+document.querySelectorAll('.gallery__image').forEach((item, i) => {
+  item.addEventListener('click', () => {
+    basicLightbox.create(`
+		<img width="1280" height="853" src="${imageOriginal[i]}">
+	`).show()
+  })
+})
